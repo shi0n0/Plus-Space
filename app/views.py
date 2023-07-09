e5825ea0846417adf096f3b9bbcdd7fe8e05bf40
shi0n0 <shiono.yui1080@gmail.com>
2023-07-10 00:01:04 +0900

    TemplateViewを使用

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
-def index(request):
-    return render(request, "app/index.html")
+class IndexView(TemplateView):
+    template_name = 'app/index.html'
