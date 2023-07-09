8178852c45b92ca9c49a0da02499d4d10bc6b9c5
shi0n0 <shiono.yui1080@gmail.com>
2023-07-09 23:36:56 +0900

    トップのルーティングを追加

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("app.api.urls")),
+    path('api/', include("app.api.urls")),
+    path('', include("app.urls")),
 ]
