ad5c7850b7ec38f2a887da5251a5b0be6170aa78
shi0n0 <shiono.yui1080@gmail.com>
2023-07-11 16:45:14 +0900

    vue.config.jsをwebpack用に改変

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,4 @@
+const { watch } = require("vue");
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
@@ -5,6 +6,14 @@ module.exports = {
   outputDir: "./dist/",
 
   chainWebpack: config => {
-    
-  }
-}
+    config.plugin("BundleTracker").use(BundleTracker, [{ filename: "./webpack-stats.json"}]);
+    config.output.filename("bundle.js");
+    config.optimization.splitChunks(false);
+    config.resolve.alias.set("__STATIC__", "static");
+    config.devServer
+    .hotOnly(true)
+    .watchOptions({ poll: 1000 })
+    .https(false)
+    .headers({ "Access-Control-Allow-Origin": ["*"] });
+  },
+};
