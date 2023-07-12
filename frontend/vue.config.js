1f1f186d60719362f9684b87d9a437eb8f42fe9b
shi0n0 <shiono.yui1080@gmail.com>
2023-07-12 22:51:42 +0900

    webpack5に合わせた記述に改変

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,19 +1,37 @@
-const { watch } = require("vue");
-const BundleTracker = require("webpack-bundle-tracker");
+const path = require('path');
+const { watch } = require('vue');
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  publicPath: "http://0.0.0.0:8080/",
-  outputDir: "./dist/",
+  publicPath: 'http://127.0.0.1:8000/',
 
   chainWebpack: config => {
-    config.plugin("BundleTracker").use(BundleTracker, [{ filename: "./webpack-stats.json"}]);
-    config.output.filename("bundle.js");
+    config.plugin('BundleTracker').use(BundleTracker, [{ filename: 'webpack-stats.json' }]);
+    config.output.filename('bundle.js');
     config.optimization.splitChunks(false);
-    config.resolve.alias.set("__STATIC__", "static");
-    config.devServer
-    .hotOnly(true)
-    .watchOptions({ poll: 1000 })
-    .https(false)
-    .headers({ "Access-Control-Allow-Origin": ["*"] });
+    config.resolve.alias.set('__STATIC__', 'static');
+  },
+
+  devServer: {
+    hot: true,
+  },
+
+  configureWebpack: {
+    resolve: {
+      extensions: ['.ts', '.js', '.vue', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+            transpileOnly: true,
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
   },
 };
