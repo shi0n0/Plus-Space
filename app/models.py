c6f4cd17d7d042658b90556145874097b4a7a2d7
shi0n0 <shiono.yui1080@gmail.com>
2023-07-17 23:41:39 +0900

    ユーザーモデル構想中

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    icon = models.ImageField(upload_to='profile_pictures/icon')
+    header = models.ImageField(upload_to='profile_pictures/header')
+    bio = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    social_media = models.CharField(max_length=100, blank=True)
