0d847d8eb7a8d38f3809c90c44e19c58108f9616
shi0n0 <shiono.yui1080@gmail.com>
2023-07-18 23:52:59 +0900

    Add Vuetify

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,25 +1,32 @@
-const BundleTracker = require('webpack-bundle-tracker');
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    publicPath: "http://0.0.0.0:8080",
-    outputDir: "./dist/",
+  publicPath: "http://0.0.0.0:8080",
+  outputDir: "./dist/",
+  transpileDependencies: ["Vuetify"],
 
-    chainWebpack: config => {
-        config.optimization.splitChunks(false)
+  chainWebpack: (config) => {
+    config.optimization.splitChunks(false);
 
-        config.plugin('BundleTracker').use(BundleTracker, [
-            {
-                filename: 'webpack-stats.json'
-            }
-        ])
-        
-        config.resolve.alias.set('__STATIC__', 'static')
+    config.plugin("BundleTracker").use(BundleTracker, [
+      {
+        filename: "webpack-stats.json",
+      },
+    ]);
 
-        config.devServer
-            .host('0.0.0.0')
-            .port(8080)
-            .hot(true)
-            .https(false)
-            .headers({'Access-Control-Allow-Origin': ['\*']})
-    }
+    config.resolve.alias.set("__STATIC__", "static");
+
+    config.devServer
+      .host("0.0.0.0")
+      .port(8080)
+      .hot(true)
+      .https(false)
+      .headers({ "Access-Control-Allow-Origin": ["*"] });
+  },
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
 };
