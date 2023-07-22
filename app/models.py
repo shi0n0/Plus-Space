aafc0fb649f7f209222b3332afe12ac74f9f7619
shi0n0 <shiono.yui1080@gmail.com>
2023-07-22 21:19:50 +0900

    AdstractBaseUserを使用した記述に変更

--- app/models.py
+++ app/models.py
@@ -1,10 +1,42 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    icon = models.ImageField(upload_to='profile_pictures/icon')
-    header = models.ImageField(upload_to='profile_pictures/header')
-    bio = models.TextField(blank=True)
-    website = models.URLField(blank=True)
-    social_media = models.CharField(max_length=100, blank=True)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Eメールは必須です")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=True)
+    name = models.CharField(max_length=30, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+class Illustration(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='illustrations/')
+    upload_date = models.DateTimeField(default=timezone.now)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
