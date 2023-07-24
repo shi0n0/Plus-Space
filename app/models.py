e67bf9d350e3bf980f5a6f0fefa20c2926621cb4
shi0n0 <shiono.yui1080@gmail.com>
2023-07-24 23:41:10 +0900

    UserモデルおよびUserManagerの改善

--- app/models.py
+++ app/models.py
@@ -1,42 +1,50 @@
+from datetime import timezone
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils import timezone
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Eメールは必須です")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+# カスタムユーザーマネージャーを定義
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("ユーザー名は必須です")
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        # ユーザー名を正規化（任意）
+        username = self.normalize_username(username)
+        
+        # ユーザーオブジェクトを作成
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
 
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True, blank=True)
-    name = models.CharField(max_length=30, blank=False)
-    is_active = models.BooleanField(default=True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('スーパーユーザーはis_staff=Trueである必要があります。')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('スーパーユーザーはis_superuser=Trueである必要があります。')
+
+        return self.create_user(username, password, **extra_fields)
+
+# カスタムユーザーモデルを定義
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    # ユーザー名（必須）
+    username = models.CharField(max_length=150, unique=True)
+    # その他のカスタムフィールドを追加する場合はここに記述する
+
+    # スーパーユーザーとして設定する権限（必須）
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-    objects = UserManager()
+    # ユーザーマネージャーを設定
+    objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    def __str__(self):
-        return self.email
-
-class Illustration(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='illustrations/')
-    upload_date = models.DateTimeField(default=timezone.now)
-    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    # ユーザー名を識別するフィールドを設定
+    USERNAME_FIELD = 'username'
 
     def __str__(self):
-        return self.title
+        return self.username
