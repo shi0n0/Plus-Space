18ab242b9fb83f28a8d1fb07294566a44266e3c5
shi0n0 <shiono.yui1080@gmail.com>
2023-07-25 23:44:29 +0900

    フロントエンド用ルーターの設定

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,19 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/register',
+    component: () => import('@/views/Register.vue'),
   },
+  {
+    path: '/login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/logout',
+    component: () => import('@/views/Logout.vue'),
+  },
+  // その他のルートを追加する場合はここに記述
 ];
 
 const router = createRouter({
