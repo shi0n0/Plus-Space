09ad9febb00051760e93e4e44070547a40fed7af
shi0n0 <shiono.yui1080@gmail.com>
2023-07-27 00:13:32 +0900

    バックエンドのAPIエンドポイントのベースURLを指定

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,13 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: '/api/', // DjangoバックエンドのAPIエンドポイントのベースURLを指定
+});
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+createApp(App).use(router).use(store).use(vuetify).provide('api', apiClient).mount("#app");
+
