5d8df4b906e33f564498c48ed1a729182fa1d9ae
shi0n0 <shiono.yui1080@gmail.com>
2023-07-28 23:45:13 +0900

    シングルクォートからダブルクォートに統一(prettier)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,21 +1,21 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
-import { loadFonts } from './plugins/webfontloader';
-import axios from 'axios';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import axios from "axios";
 
 // @ts-ignore
 const apiClient = axios.create({
-  baseURL: '/api/', // DjangoバックエンドのAPIエンドポイントのベースURLを指定
+  baseURL: "/api/", // DjangoバックエンドのAPIエンドポイントのベースURLを指定
 });
 
 const app = createApp(App);
 app.config.globalProperties.axios = apiClient; // axiosをVueインスタンスのプロパティとして追加
 app.use(router).use(store).use(vuetify);
-app.provide('api', apiClient); // APIクライアントをprovide
+app.provide("api", apiClient); // APIクライアントをprovide
 
-app.mount('#app');
+app.mount("#app");
 
 loadFonts();
