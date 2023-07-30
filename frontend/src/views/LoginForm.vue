05f7395b582c6a3d5feb55f0dfcd9c8630356c8a
shi0n0 <shiono.yui1080@gmail.com>
2023-07-30 22:40:42 +0900

    RegisterFormと同様のレンプレートのLoginFormを作成

--- frontend/src/views/LoginForm.vue
+++ frontend/src/views/LoginForm.vue
@@ -1,3 +1,40 @@
 <template>
-  <div></div>
-</template>
+    <form @submit.prevent="loginUser">
+      <label for="username">ユーザー名:</label>
+      <input type="text" v-model="username" required>
+  
+      <label for="password">パスワード:</label>
+      <input type="password" v-model="password" required>
+  
+      <button type="submit">ログイン</button>
+    </form>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import axios from 'axios';
+  
+  export default defineComponent({
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      async loginUser() {
+        try {
+        // @ts-ignore
+          const response = await axios.post('/api/login/', {
+            username: this.username,
+            password: this.password,
+          });
+          console.log(response.data); // ログイン成功時のレスポンスを表示（任意）
+        } catch (error: any) {
+          console.error(error.response.data); // エラーレスポンスを表示（任意）
+        }
+      },
+    },
+  });
+  </script>
+  
