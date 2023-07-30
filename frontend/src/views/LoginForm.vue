10c3000df052d7a47edd9653981f4390ce75a59e
shi0n0 <shiono.yui1080@gmail.com>
2023-07-31 00:05:15 +0900

    ログインの状態管理(vuex)を追加

--- frontend/src/views/LoginForm.vue
+++ frontend/src/views/LoginForm.vue
@@ -1,40 +1,32 @@
-<template>
-    <form @submit.prevent="loginUser">
-      <label for="username">ユーザー名:</label>
-      <input type="text" v-model="username" required>
-  
-      <label for="password">パスワード:</label>
-      <input type="password" v-model="password" required>
-  
-      <button type="submit">ログイン</button>
-    </form>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import axios from 'axios';
-  
-  export default defineComponent({
-    data() {
-      return {
-        username: '',
-        password: '',
-      };
-    },
-    methods: {
-      async loginUser() {
-        try {
+<script lang="ts">
+import { defineComponent, ref, inject } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    const api = inject<any>('api'); // Vuexストアを注入
+    const username = ref('');
+    const password = ref('');
+
+    const loginUser = async () => {
+      try {
         // @ts-ignore
-          const response = await axios.post('/api/login/', {
-            username: this.username,
-            password: this.password,
-          });
-          console.log(response.data); // ログイン成功時のレスポンスを表示（任意）
-        } catch (error: any) {
-          console.error(error.response.data); // エラーレスポンスを表示（任意）
-        }
-      },
-    },
-  });
-  </script>
-  
+        const response = await axios.post('/api/login/', {
+          username: username.value,
+          password: password.value,
+        });
+        console.log(response.data); // ログイン成功時のレスポンスを表示（任意）
+        api.setLoginState(true); // ログイン状態をVuexストアにセット
+      } catch (error: any) {
+        console.error(error.response.data); // エラーレスポンスを表示（任意）
+      }
+    };
+
+    return {
+      username,
+      password,
+      loginUser,
+    };
+  },
+});
+</script>
