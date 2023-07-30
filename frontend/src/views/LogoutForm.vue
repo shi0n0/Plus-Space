bc8f3d43a784e24ac2662707abac637b493373f7
shi0n0 <shiono.yui1080@gmail.com>
2023-07-31 00:05:41 +0900

    ログアウトのviewと状態管理(vuex)を追加

--- frontend/src/views/LogoutForm.vue
+++ frontend/src/views/LogoutForm.vue
@@ -1,3 +1,25 @@
-<template>
-  <div></div>
-</template>
+<script lang="ts">
+import { defineComponent, inject } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    const api = inject<any>('api'); // Vuexストアを注入
+
+    const logoutUser = async () => {
+      try {
+        // @ts-ignore
+        const response = await axios.post('/api/logout/');
+        console.log(response.data); // ログアウト成功時のレスポンスを表示（任意）
+        api.setLoginState(false); // ログイン状態をVuexストアにセット
+      } catch (error: any) {
+        console.error(error.response.data); // エラーレスポンスを表示（任意）
+      }
+    };
+
+    return {
+      logoutUser,
+    };
+  },
+});
+</script>
