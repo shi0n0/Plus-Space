de41ed423562552875fff38a2c547fb128ca0119
shi0n0 <shiono.yui1080@gmail.com>
2023-07-30 23:56:28 +0900

    ログインのステートの設定

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,17 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    isLoggedIn: false, // ログイン状態を管理するステート
+  },
+  mutations: {
+    SET_LOGIN_STATE(state, isLoggedIn) {
+      state.isLoggedIn = isLoggedIn;
+    },
+  },
+  actions: {
+    setLoginState({ commit }, isLoggedIn) {
+      commit('SET_LOGIN_STATE', isLoggedIn);
+    },
+  },
 });
