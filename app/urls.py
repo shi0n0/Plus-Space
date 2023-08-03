0f4c613b9c53f8a0ab9411124f30b0bc88a2b667
shi0n0 <shiono.yui1080@gmail.com>
2023-08-03 23:55:36 +0900

    apiディレクトリを削除してルートディレクトリに統一

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import re_path
-from . import views
+from django.urls import path
+from .views import SampleAPIView, register_user, login_user, logout_user
 
 urlpatterns = [
-    re_path('', views.IndexView.as_view(), name="index"),
+    path('register/', register_user, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import status
+from rest_framework.views import APIView
+from django.contrib.auth import login, logout, authenticate
+from .serializer import CustomUserSerializer
+from django.contrib.auth.decorators import login_required
 
-class IndexView(TemplateView):
-    template_name = 'app/index.html'
+
+
+# ユーザー登録APIビュー
+@api_view(['POST'])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# ユーザーログインAPIビュー
+@api_view(['POST'])
+def login_user(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({'message': 'ログインに成功しました。'}, status=status.HTTP_200_OK)
+        return Response({'message': '無効なログイン情報です。'}, status=status.HTTP_401_UNAUTHORIZED)
+
+# ユーザーログアウトAPIビュー
+@login_required
+@api_view(['POST'])
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return Response({'message': 'ログアウトしました。'}, status=status.HTTP_200_OK)
