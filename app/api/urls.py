37d02eb0a519aa4d051ce635e3730c7bfbc2efa4
shi0n0 <shiono.yui1080@gmail.com>
2023-08-10 03:17:38 +0900

    apiディレクトリからルートディレクトリに統合

--- app/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register_user, login_user, logout_user
-
-urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
-]
